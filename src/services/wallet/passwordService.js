2075d8e703fcc32993ee4898de6d108b8cc58e65
darius <datle@incognito.org>
2021-06-04 10:54:37 +0700

    use expo-secure-storage for encryption key; move AES encrypt to gomobile

--- src/services/wallet/passwordService.js
+++ src/services/wallet/passwordService.js
@@ -1,6 +1,9 @@
 import { CONSTANT_CONFIGS, CONSTANT_KEYS } from '@src/constants';
 import storage from '@src/services/storage';
-import CryptoJS from 'crypto-js';
+import { getItemAsync, setItemAsync } from 'expo-secure-store';
+import { byteToHexString } from 'incognito-chain-web-js/build/wallet';
+import { misc, codec } from 'incognito-chain-web-js/lib/privacy/sjcl';
+import { randomBytes } from 'react-native-randombytes';
 
 const PASSWORD_DURATION_IN_MS = 7 * 24 * 3600 * 1000; // 7 days
 
@@ -30,7 +33,27 @@ export async function getPassphrase() {
   //   return CONSTANT_CONFIGS.PASSPHRASE_WALLET_DEFAULT;
   // }
 
-  return CONSTANT_CONFIGS.PASSPHRASE_WALLET_DEFAULT;
+  // TODO : password expiry
+  let password = CONSTANT_CONFIGS.PASSPHRASE_WALLET_DEFAULT;
+  const ACCESS_KEY = CONSTANT_KEYS.SALT_KEY || 'default-wallet-salt';
+  try {
+    let salt = await getItemAsync(ACCESS_KEY);
+    if (!salt) {
+      // generate a new wallet encryption key
+      const raw = randomBytes(16);
+      salt = byteToHexString(raw);
+      await setItemAsync(ACCESS_KEY, salt);
+      console.log('PASSWORD-SERVICE : Generated new salt for wallet key in SecureStorage', salt);
+    } else { 
+      console.log('PASSWORD-SERVICE : Load wallet salt', salt);
+    }
+    let aesKey = misc.pbkdf2(password, salt, null, 128);
+    aesKey = codec.hex.fromBits(aesKey);
+    return aesKey;
+  } catch (e) {
+    console.log('error accessing SecureStorage', e);
+    throw e;
+  }
 }
 
 export async function hasPassword() {
@@ -40,11 +63,8 @@ export async function hasPassword() {
 export function savePassword(pass) {
   try {
     const expired = Date.now() + PASSWORD_DURATION_IN_MS;
-    const toBeSaved = CryptoJS.AES.encrypt(
-      `${pass}:${expired}`,
-      CONSTANT_CONFIGS.PASSWORD_SECRET_KEY
-    ).toString();
-    return storage.setItem(CONSTANT_KEYS.PASSPHRASE_KEY, toBeSaved);
+    const toBeSaved = `${pass}:${expired}`;
+    return setItemAsync(CONSTANT_KEYS.PASSPHRASE_KEY, toBeSaved);
   } catch {
     throw new Error('Can not save your password, please try again');
   }
