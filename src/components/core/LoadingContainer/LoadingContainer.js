950568f9fff461cc92973f4749da8b4a44612959
phathoang@incognito.org <phathoang@incognito.org>
2021-07-03 22:15:08 +0700

    Update warning message when load master key

--- src/components/core/LoadingContainer/LoadingContainer.js
+++ src/components/core/LoadingContainer/LoadingContainer.js
@@ -12,16 +12,18 @@ const styled = StyleSheet.create({
 });
 
 const LoadingContainer = (props) => {
-  const { containerStyled } = props;
+  const { containerStyled, custom } = props;
   return (
     <View style={[styled.container, containerStyled]}>
       <ActivityIndicator size={props?.size || 'small'} />
+      {custom && custom}
     </View>
   );
 };
 
 LoadingContainer.propTypes = {
   containerStyled: null,
+  custom: null,
 };
 
 export default React.memo(LoadingContainer);
