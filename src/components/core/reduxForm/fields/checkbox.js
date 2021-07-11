a30f08de6a50f4650de76170ea466c8fabced34d
hiennguyen-me <nguyenthithuhien249@gmail.com>
2021-07-11 18:47:41 +0700

    add checkbox redux field

---
+++ src/components/core/reduxForm/fields/checkbox.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { CheckBox } from '@src/components/core';
+import createField from './createField';
+
+const renderCustomField = ({ input, ...props }) => {
+  const { value } = input;
+  const { onPress, title } = props;
+  let _value = false;
+
+  try {
+    _value = JSON.parse(value);
+  } catch (e) {
+    _value = false;
+  }
+  
+  return (
+    <CheckBox
+      {...props}
+      title={title}
+      checked={_value}
+      onPress={() => typeof onPress === 'function' && onPress(_value)}
+    />
+  );
+};
+
+const CheckboxField = createField({
+  fieldName: 'CheckboxField',
+  render: renderCustomField,
+});
+
+export default CheckboxField;
