35d1e6d75c3d4e796bfa1effed094bee79ac7a91
hiennguyen-me <nguyenthithuhien249@gmail.com>
2021-07-19 17:10:18 +0700

    Merge code from privacy v2 and resolve conflicts

--- ios/Gomobile.framework/Headers/Gomobile.objc.h
+++ ios/Gomobile.framework/Headers/Gomobile.objc.h
@@ -314,6 +314,7 @@
 @property (nonatomic) NSData* _Nullable info;
 @end
 
+FOUNDATION_EXPORT const int64_t GomobileAES_BLOCK_SIZE;
 FOUNDATION_EXPORT const int64_t GomobileCustomTokenCrossShard;
 FOUNDATION_EXPORT const int64_t GomobileCustomTokenInit;
 FOUNDATION_EXPORT const int64_t GomobileCustomTokenTransfer;
@@ -335,6 +336,10 @@ FOUNDATION_EXPORT NSString* _Nonnull const GomobileTxTokenConversionType;
 // skipped variable PRVCoinID with unsupported type: incognito-chain/common.Hash
 
 @end
+
+FOUNDATION_EXPORT NSString* _Nonnull GomobileAesDecrypt(NSString* _Nullable args, NSError* _Nullable* _Nullable error);
+
+FOUNDATION_EXPORT NSString* _Nonnull GomobileAesEncrypt(NSString* _Nullable args, NSError* _Nullable* _Nullable error);
 
 FOUNDATION_EXPORT NSString* _Nonnull GomobileCreateCoin(NSString* _Nullable paramStr, NSError* _Nullable* _Nullable error);
 
@@ -387,6 +392,8 @@ FOUNDATION_EXPORT NSString* _Nonnull GomobileScalarMultBase(NSString* _Nullable 
 
 // skipped function ScalarToBytes with unsupported parameter or return types
 
+
+FOUNDATION_EXPORT NSString* _Nonnull GomobileSetShardCount(NSString* _Nullable p0, int64_t num, NSError* _Nullable* _Nullable error);
 
 // skipped function SignNoPrivacy with unsupported parameter or return types
 
