b2baadad1653abe75cdbdcdb164417851c838ca1
phathoang@incognito.org <phathoang@incognito.org>
2021-07-28 15:41:44 +0700

    Verifier tx

--- src/screens/Wallet/features/History/History.js
+++ src/screens/Wallet/features/History/History.js
@@ -1,6 +1,5 @@
 import { Dimensions, View, Text, Clipboard } from 'react-native';
 import { compose } from 'recompose';
-// import { ButtonBasic } from '@src/components/Button';
 import {
   ScrollView,
   Toast,
@@ -14,14 +13,11 @@ import {
 } from '@src/redux/selectors/history';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { devSelector } from '@src/screens/Dev';
-// import { CONSTANT_CONFIGS, CONSTANT_KEYS } from '@src/constants';
 import { CopyIcon, OpenUrlIcon } from '@src/components/Icons';
-// import { selectedPrivacySelector } from '@src/redux/selectors';
 import { Header } from '@src/components';
 import { withLayout_2 } from '@src/components/Layout';
 import { QrCodeAddressDefault } from '@src/components/QrCodeAddress';
-import { BtnChevron, BtnResume } from '@src/components/Button';
+import { BtnChevron, BtnCopy, BtnResume } from '@src/components/Button';
 import HTML from 'react-native-render-html';
 import LinkingService from '@src/services/linking';
 import { ExHandler } from '@src/services/exception';
@@ -147,22 +143,23 @@ const Hook = React.memo((props) => {
 const History = () => {
   const factories = useSelector(historyDetailFactoriesSelector);
   const { tx, fetching } = useSelector(historyDetailSelector);
-  // const selectedPrivacy = useSelector(selectedPrivacySelector.selectedPrivacy);
-  // const dev = useSelector(devSelector);
   const dispatch = useDispatch();
-  // const keySave = CONSTANT_KEYS.DEV_TEST_TOGGLE_HISTORY_DETAIL;
-  // const toggleTxHistoryDetail = global.isDebug() && dev[keySave];
   if (factories.length === 0) {
     return null;
   }
   const handleRefresh = () => dispatch(actionFetchTx());
-  // const onCopyData = () => {
-  //   Clipboard.setString(JSON.stringify(tx));
-  //   Toast.showSuccess('Copied');
-  // };
+  const onCopyData = () => {
+    Clipboard.setString(JSON.stringify(tx));
+    Toast.showSuccess('Copied');
+  };
   return (
     <View style={styled.container}>
-      <Header title="Transaction detail" />
+      <Header
+        title="Transaction detail"
+        customHeaderTitle={
+          <BtnCopy style={{ marginLeft: 10 }} onPress={onCopyData} />
+        }
+      />
       <ScrollView
         refreshControl={
           <RefreshControl refreshing={fetching} onRefresh={handleRefresh} />
@@ -180,13 +177,6 @@ const History = () => {
             min={tx?.minShield}
           />
         )}
-        {/* {toggleTxHistoryDetail && (
-          <ButtonBasic
-            title="Copy"
-            btnStyle={{ marginTop: 30 }}
-            onPress={onCopyData}
-          />
-        )} */}
         <View style={{ height: 50 }} />
       </ScrollView>
     </View>
