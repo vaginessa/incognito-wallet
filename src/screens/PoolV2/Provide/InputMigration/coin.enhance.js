8938f15dd675ca60dc6a3a3da046a204578dfdbe
thach4567 <thach4567@gmail.com>
2021-09-30 13:26:13 +0700

    update min migrate amount

--- src/screens/PoolV2/Provide/InputMigration/coin.enhance.js
+++ src/screens/PoolV2/Provide/InputMigration/coin.enhance.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { useNavigationParam } from 'react-navigation-hooks';
 import { MAX_FEE_PER_TX } from '@components/EstimateFee/EstimateFee.utils';
 import { COINS } from '@src/constants';
+import { PRV_ID } from '@src/screens/DexV2/constants';
 
 const withCoinData = WrappedComp => (props) => {
   const data = useNavigationParam('data');
   const coins = useNavigationParam('coins');
   const coin = data?.coin;
+
+  // get min lock amount of prv
+  let minLock = coins.find(i => i?.id === PRV_ID && i?.locked);
+  if (!minLock) {
+    minLock = coin;
+  }
 
   return (
     <WrappedComp
@@ -17,7 +24,7 @@ const withCoinData = WrappedComp => (props) => {
         coin,
         inputToken: coin,
         inputBalance: data.balance,
-        inputMin: coin.min || MAX_FEE_PER_TX,
+        inputMin: minLock.min || MAX_FEE_PER_TX,
         fee: 0,
         feeToken: COINS.PRV,
       }}
