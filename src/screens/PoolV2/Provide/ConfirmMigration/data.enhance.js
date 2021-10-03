f6177ee7709647dc52e8df26cfbb4c71d5e2b9d5
0xkraken <hien.n@autonomous.nyc>
2021-10-03 12:25:58 +0700

    Update get lock data from api get history

--- src/screens/PoolV2/Provide/ConfirmMigration/data.enhance.js
+++ src/screens/PoolV2/Provide/ConfirmMigration/data.enhance.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigationParam } from 'react-navigation-hooks';
-import formatUtil from '@utils/format';
+import formatUtil, {LONG_DATE_TIME_FORMAT} from '@utils/format';
 
 const withData = WrappedComp => (props) => {
   const data        = useNavigationParam('data');
@@ -15,7 +15,7 @@ const withData = WrappedComp => (props) => {
   
   let unlockTime = new Date();
   unlockTime.setMonth(unlockTime.getMonth() + coin.lockTime);
-  const unlockTimeFormat = formatUtil.formatDateTime(unlockTime, 'DD MMM YYYY HH:mm A');
+  const unlockTimeFormat = formatUtil.formatDateTime(unlockTime, LONG_DATE_TIME_FORMAT);
 
   return (
     <WrappedComp
