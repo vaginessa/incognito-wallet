797ed6b3c4eda228236276a8e87728405ebc96f0
SangNX063 <sang@incognito.org>
2021-10-19 21:19:42 +0700

    update

---
+++ src/screens/SelectAccount/SelectAccount.js
@@ -0,0 +1,67 @@
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useSearchBox } from '@src/components/Header';
+import { useNavigationParam } from 'react-navigation-hooks';
+import { flatMap, groupBy } from 'lodash';
+import includes from 'lodash/includes';
+import { listAllMasterKeyAccounts } from '@src/redux/selectors/masterKey';
+import accountService from '@services/wallet/accountService';
+import GroupItem from '@screens/SelectAccount/GroupItem';
+import AccountItem from '@screens/SelectAccount/AccountItem';
+import MainLayout from '@components/MainLayout';
+
+const SelectAccount = () => {
+  const ignoredAccounts = useNavigationParam('ignoredAccounts') || [];
+  const handleSelectedAccount = useNavigationParam('handleSelectedAccount');
+  const listAccount = useSelector(listAllMasterKeyAccounts);
+  const [result, keySearch] = useSearchBox({
+    data: listAccount,
+    handleFilter: () => [
+      ...listAccount.filter(
+        (account) =>
+          !ignoredAccounts.includes(
+            accountService.getAccountName(account).toLowerCase(),
+          ) &&
+          includes(
+            accountService.getAccountName(account).toLowerCase(),
+            keySearch,
+          ),
+      ),
+    ],
+  });
+
+  const groupAccounts = useMemo(() => {
+    if (result && result.length > 0) {
+      const groupedMasterKeys = groupBy(result, (item) => item.MasterKeyName);
+      return flatMap(groupedMasterKeys, (child, key) => ({
+        name: key,
+        child,
+      }));
+    }
+
+    return [];
+  }, [result, result.length]);
+
+  return (
+    <MainLayout header="Search keychains" canSearch scrollable>
+      {groupAccounts.map((item) => (
+        <GroupItem
+          name={item.name}
+          key={item.name}
+          child={item.child.map((account) => (
+            <AccountItem
+              key={account?.FullName}
+              accountName={account.AccountName}
+              PaymentAddress={account.PaymentAddress}
+              PrivateKey={account.PrivateKey}
+              MasterKeyName={account.MasterKeyName}
+              handleSelectedAccount={handleSelectedAccount}
+            />
+          ))}
+        />
+      ))}
+    </MainLayout>
+  );
+};
+
+export default SelectAccount;
