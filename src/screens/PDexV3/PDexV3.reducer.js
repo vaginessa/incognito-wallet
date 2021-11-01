7b7bf4e0cdf8bd5dc318a24801d52b0eee90686b
SangNX063 <sang@incognito.org>
2021-11-01 14:03:45 +0700

    remove screen

--- src/screens/PDexV3/PDexV3.reducer.js
+++ src/screens/PDexV3/PDexV3.reducer.js
@@ -6,9 +6,6 @@ import { swapReducer as swap } from './features/Swap';
 import { orderLimitReducer as orderLimit } from './features/OrderLimit';
 import { tradeReducer as trade } from './features/Trade';
 import { chartReducer as chart } from './features/Chart';
-import { contributeHistoriesReducer as contributeHistories } from './features/ContributeHistories';
-import { removePoolHistoriesReducer as removePoolHistories } from './features/RemovePoolHistories';
-import { withdrawRewardHistoriesReducer as withdrawRewardHistories } from './features/WithdrawRewardHistories';
 import { stakingReducer as staking } from './features/Staking';
 import { liquidityReducer as liquidity } from './features/Liquidity';
 import { pairsReducer as pairs } from './features/PairList';
@@ -22,9 +19,6 @@ const pDexV3Reducer = combineReducers({
   orderLimit,
   trade,
   chart,
-  contributeHistories,
-  removePoolHistories,
-  withdrawRewardHistories,
   staking,
   liquidity,
   liquidityHistory,
