4a921985a1d99ac4bf04e1213ebd52bd5dc0be07
phathoang@incognito.org <phathoang@incognito.org>
2021-11-12 08:02:15 +0700

    push code

--- src/components/Token/Token.shared.js
+++ src/components/Token/Token.shared.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 import incognito from '@assets/images/new-icons/incognito.png';
-import ImageCached from '@src/components/ImageCached';
+// import ImageCached from '@src/components/ImageCached';
 
 export const Icon = React.memo((props) => {
   const { iconUrl, style } = props;
   return (
-    <ImageCached
+    <Image
       style={[{ width: 20, height: 20 }, style]}
-      uri={iconUrl}
-      defaultImage={incognito}
+      source={{ uri: iconUrl }}
+      defaultSource={incognito}
     />
   );
+  // return (
+  //   <ImageCached
+  //     style={[{ width: 20, height: 20 }, style]}
+  //     uri={iconUrl}
+  //     defaultImage={incognito}
+  //   />
+  // );
 });
 
 Icon.defaultProps = {
-  style: null
+  style: null,
 };
 
 Icon.propTypes = {
   iconUrl: PropTypes.string.isRequired,
-  style: PropTypes.any
+  style: PropTypes.any,
 };
