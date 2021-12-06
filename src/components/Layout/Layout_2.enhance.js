3ec3394120bf729babf6fd147e5d0589ecd0df71
SangNX063 <sang@incognito.org>
2021-12-06 16:05:04 +0700

    update ui

--- src/components/Layout/Layout_2.enhance.js
+++ src/components/Layout/Layout_2.enhance.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import ErrorBoundary from '@src/components/ErrorBoundary';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { COLORS } from '@src/styles';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import withLazy from '@components/LazyHoc/LazyHoc';
-import {compose} from 'recompose';
+import { compose } from 'recompose';
+import { useSelector } from 'react-redux';
+import { colorsSelector } from '@src/theme/theme.selector';
+import { background1Screen } from '@components/core/StatusBar/StatusBar';
+import { currentScreenSelector } from '@screens/Navigation';
+import { View } from '@components/core';
 
 const styled = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLORS.white,
   },
   wrapper: {
-    // paddingHorizontal: 25,
     flex: 1,
     paddingBottom: 0,
   },
 });
 
 const enhance = (WrappedComp) => (props) => {
+  const colors = useSelector(colorsSelector);
+  const currentScreen = useSelector(currentScreenSelector);
+  const _background = React.useMemo(() => {
+    if (background1Screen.includes(currentScreen)) {
+      return colors.background1;
+    }
+    return colors.background2;
+  }, [currentScreen]);
   return (
     <ErrorBoundary>
-      <SafeAreaView style={[styled.container, props?.containerStyled]}>
+      <SafeAreaView style={[styled.container, { backgroundColor: _background }, props?.containerStyled]}>
         <View style={[styled.wrapper, props?.wrapperStyled]}>
           <WrappedComp {...props} />
         </View>
