fa370648890cb66dfad8430f2d41b4202ec83e7b
SangNX063 <sang@incognito.org>
2021-12-15 15:04:32 +0700

    update ui

--- src/components/core/Image/Component.js
+++ src/components/core/Image/Component.js
@@ -3,7 +3,6 @@ import { ImageProps } from 'react-native';
 import styled from 'styled-components/native';
 
 const StyledImage = styled.Image`
-  tint-color: ${({ theme }) => theme.text1};
 `;
 
 const Image = (props: ImageProps) => <StyledImage {...props} />;
