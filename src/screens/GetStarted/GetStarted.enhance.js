a11490ba89e42ab580793cd52d2925e28e90f177
SangNX063 <sang@incognito.org>
2021-12-15 16:50:57 +0700

    update ui

--- src/screens/GetStarted/GetStarted.enhance.js
+++ src/screens/GetStarted/GetStarted.enhance.js
@@ -81,7 +81,7 @@ const enhance = (WrappedComp) => (props) => {
     await setLoading(false);
     console.timeEnd('CONFIGS_APP');
     if (!hasError) {
-      navigation.navigate(routeNames.PoolV2);
+      navigation.navigate(routeNames.MainTabBar);
     }
   };
 
