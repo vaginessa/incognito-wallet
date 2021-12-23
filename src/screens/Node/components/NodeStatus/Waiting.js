5a5aeee5f67d2ab5f07663e4c2e033476b9ae42c
0xkraken <hien.n@autonomous.nyc>
2021-12-23 10:45:10 +0700

    Update Node screens

--- src/screens/Node/components/NodeStatus/Waiting.js
+++ src/screens/Node/components/NodeStatus/Waiting.js
@@ -1,4 +1,5 @@
 import { Text, View, TouchableOpacity } from '@components/core';
+import { Text4 } from '@components/core/Text';
 import { COLORS, FONT } from '@src/styles';
 import React from 'react';
 import linkingService from '@services/linking';
@@ -7,9 +8,9 @@ import styles from './style';
 
 const Waiting = () => (
   <View style={styles.container}>
-    <Text style={[styles.desc]}>This Node is currently waiting to be selected to create blocks and earn rewards. All Nodes have an equal chance of selection. Numbers may vary in the short-term, but will even out over time through random uniform distribution.</Text>
+    <Text4 style={[styles.desc]}>This Node is currently waiting to be selected to create blocks and earn rewards. All Nodes have an equal chance of selection. Numbers may vary in the short-term, but will even out over time through random uniform distribution.</Text4>
     <TouchableOpacity onPress={() => linkingService.openCommunityUrl(CONSTANT_URLS.VALIDATOR_LIFECYCLE)}>
-      <Text style={{color: COLORS.blue5, marginTop: 20, fontFamily: FONT.NAME.bold }}>
+      <Text style={{color: COLORS.colorBlue, marginTop: 20, fontFamily: FONT.NAME.bold }}>
         Learn about the validator lifecycle
       </Text>
     </TouchableOpacity>
