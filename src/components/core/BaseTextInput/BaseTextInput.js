03c4c4e6c549dca63f4b31dde27611455a088837
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-01-05 16:17:23 +0700

    push code

--- src/components/core/BaseTextInput/BaseTextInput.js
+++ src/components/core/BaseTextInput/BaseTextInput.js
@@ -5,11 +5,65 @@ import styled from 'styled-components/native';
 import { isAndroid } from '@src/utils/platform';
 import { colorsSelector } from '@src/theme/theme.selector';
 import { useSelector } from 'react-redux';
+import debounce from 'lodash/debounce';
 import styles from './styles';
 
 const StyledInput = styled.TextInput`
   color: ${({ theme }) => theme.text1};
 `;
+
+// A custome hook for position of cursor's text input on Android OS
+const useCursorInputTextForAndroid = ({ value, onBlur, onFocus }) => {
+  const [selection, setSelection] = React.useState(undefined);
+  const [isFocused, setIsFocused] = React.useState(false);
+  const handleSetSelectionForAnroid = React.useCallback(
+    (selection, value) => {
+      if (isAndroid() && !!value) {
+        setSelection(selection);
+        setTimeout(() => {
+          setSelection(undefined);
+        }, 100);
+      }
+    },
+    [value, selection],
+  );
+  const handleOnBlur = () => {
+    setIsFocused(false);
+    if (typeof onBlur === 'function') {
+      onBlur();
+    }
+    handleSetSelectionForAnroid({ start: 0, end: 0 }, value);
+  };
+
+  const handleOnFocus = async () => {
+    setIsFocused(true);
+    if (typeof onFocus === 'function') {
+      onFocus();
+    }
+    handleSetSelectionForAnroid(
+      { start: value?.length, end: value?.length },
+      value,
+    );
+  };
+  const debounceHandleSelection = debounce(
+    React.useCallback((value) => {
+      handleSetSelectionForAnroid({ start: 0, end: 0 }, value);
+    }, []),
+    100,
+  );
+  React.useEffect(() => {
+    if (isFocused || !value) {
+      return;
+    }
+    debounceHandleSelection(value);
+  }, [value, isFocused]);
+  return {
+    selection,
+    setSelection,
+    handleOnBlur,
+    handleOnFocus,
+  };
+};
 
 const BaseTextInput = (props: TextInputProps) => {
   const {
@@ -22,30 +76,12 @@ const BaseTextInput = (props: TextInputProps) => {
     ...rest
   } = props;
   const colors = useSelector(colorsSelector);
-  const [selection, setSelection] = React.useState(undefined);
-  const handleOnBlur = () => {
-    if (typeof onBlur === 'function') {
-      onBlur();
-    }
-    if (isAndroid() && !!value) {
-      setSelection({ ...selection, start: 0, end: 0 });
-      setTimeout(() => {
-        setSelection(undefined);
-      }, 100);
-    }
-  };
-
-  const handleOnFocus = async () => {
-    if (typeof onFocus === 'function') {
-      onFocus();
-    }
-    if (isAndroid() && !!value) {
-      setSelection(
-        Object.assign({}, { start: value?.length, end: value?.length }),
-      );
-      setTimeout(() => {
-        setSelection(undefined);
-      }, 100);
+  const { selection, setSelection, handleOnBlur, handleOnFocus } =
+    useCursorInputTextForAndroid({ value, onBlur, onFocus });
+  const handleChangeText = (text) => {
+    setSelection(undefined);
+    if (typeof onChangeText === 'function') {
+      onChangeText(text);
     }
   };
   return (
@@ -61,12 +97,7 @@ const BaseTextInput = (props: TextInputProps) => {
       selection={isAndroid() && !!selection ? selection : undefined}
       onBlur={handleOnBlur}
       onFocus={handleOnFocus}
-      onChangeText={(text) => {
-        setSelection(undefined);
-        if (typeof onChangeText === 'function') {
-          onChangeText(text);
-        }
-      }}
+      onChangeText={handleChangeText}
       {...rest}
     />
   );
