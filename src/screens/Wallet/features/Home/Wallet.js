87a13909decb01886fa17c46ad5cedc29be4051e
SangNX063 <sang@incognito.org>
2022-01-05 09:47:46 +0700

    Improve some function: - Node - Covert + consolate - Liquidity show network name

--- src/screens/Wallet/features/Home/Wallet.js
+++ src/screens/Wallet/features/Home/Wallet.js
@@ -4,12 +4,14 @@ import Extra from '@screens/Wallet/features/Home/Wallet.extra';
 import Followed from '@screens/Wallet/features/Home/Wallet.followed';
 import withNews from '@screens/News/News.enhance';
 import { compose } from 'recompose';
+import StreamLineBottomBar from '@screens/Streamline/features/StreamLineBottomBar';
 
 const Wallet = React.memo(() => {
   return (
     <>
       <Extra />
       <Followed />
+      <StreamLineBottomBar />
     </>
   );
 });
