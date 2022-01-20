ce43042161ceb1b9231f302ed97117eddfe33311
TN <thu@incognito.org>
2022-01-20 17:23:39 +0700

    fix bug status color history

--- src/redux/utils/history.js
+++ src/redux/utils/history.js
@@ -16,16 +16,22 @@ export const {
 } = ACCOUNT_CONSTANT;
 
 const PORTAL_UNSHIELD_STATUS_DETAIL = {
-  [STATUS_CODE_UNSHIELD_PORTAL.PENDING]: 'The unshielding request is waiting for processing.',
-  [STATUS_CODE_UNSHIELD_PORTAL.PROCESSING]: 'The unshielding request is processing.',
-  [STATUS_CODE_UNSHIELD_PORTAL.COMPLETE]: 'The public token has been sent to your external address.',
-  [STATUS_CODE_UNSHIELD_PORTAL.REFUND]: 'The unshielding request was refunded to your account. Please try again.'
+  [STATUS_CODE_UNSHIELD_PORTAL.PENDING]:
+    'The unshielding request is waiting for processing.',
+  [STATUS_CODE_UNSHIELD_PORTAL.PROCESSING]:
+    'The unshielding request is processing.',
+  [STATUS_CODE_UNSHIELD_PORTAL.COMPLETE]:
+    'The public token has been sent to your external address.',
+  [STATUS_CODE_UNSHIELD_PORTAL.REFUND]:
+    'The unshielding request was refunded to your account. Please try again.',
 };
 
 const PORTAL_SHIELD_STATUS_DETAIL = {
-  [STATUS_CODE_SHIELD_PORTAL.PENDING]: 'The shielding transaction is waiting for confirmation.',
+  [STATUS_CODE_SHIELD_PORTAL.PENDING]:
+    'The shielding transaction is waiting for confirmation.',
   [STATUS_CODE_SHIELD_PORTAL.PROCESSING]: 'The shielding transaction has ', // + 2/6 block confirmations.
-  [STATUS_CODE_SHIELD_PORTAL.FAILED]: 'The shielding request is rejected because the shielding amount is smaller than the minimum amount.',
+  [STATUS_CODE_SHIELD_PORTAL.FAILED]:
+    'The shielding request is rejected because the shielding amount is smaller than the minimum amount.',
   [STATUS_CODE_SHIELD_PORTAL.SUCCESS]: '',
 };
 
@@ -37,44 +43,52 @@ export const TX_STATUS_COLOR = {
   [TX_STATUS.TXSTATUS_SUCCESS]: COLORS.green,
 };
 
+export const CENTRALIZED_ARRAY = [0];
+export const DECENTRALIZE_ARRAY = [1, 2, 3, 4];
+
 export const getStatusColorShield = (history) => {
   let statusColor = '';
   try {
     new Validator('getStatusColorShield-history', history).required().object();
     const { decentralized, status } = history;
-    switch (decentralized) {
-    case 0: {
-      // centralized
-      if (STATUS_CODE_SHIELD_CENTRALIZED.COMPLETE === status || STATUS_CODE_SHIELD_CENTRALIZED.COMPLETE.includes(status)) {
+    // Case1: Check centralized
+    if (CENTRALIZED_ARRAY.includes(decentralized)) {
+      if (
+        STATUS_CODE_SHIELD_CENTRALIZED.COMPLETE.includes(status) ||
+        STATUS_CODE_SHIELD_CENTRALIZED.COMPLETE === status
+      ) {
         statusColor = COLORS.green;
-      } else if (STATUS_CODE_SHIELD_CENTRALIZED.TIMED_OUT === status || STATUS_CODE_SHIELD_CENTRALIZED.TIMED_OUT.includes(status)) {
+      } else if (
+        STATUS_CODE_SHIELD_CENTRALIZED.TIMED_OUT.includes(status) ||
+        STATUS_CODE_SHIELD_CENTRALIZED.TIMED_OUT === status
+      ) {
         statusColor = COLORS.orange;
       } else {
         statusColor = COLORS.lightGrey36;
       }
-      break;
     }
-    case 1:
-    case 2:
-    case 3:
-    case 4: {
-      // decetralized
-      if (STATUS_CODE_SHIELD_DECENTRALIZED.COMPLETE === status || STATUS_CODE_SHIELD_DECENTRALIZED.COMPLETE.includes(status)) {
+    // Case 2: Check decentralized
+    else if (DECENTRALIZE_ARRAY.includes(decentralized)) {
+      if (
+        STATUS_CODE_SHIELD_DECENTRALIZED.COMPLETE.includes(status) ||
+        STATUS_CODE_SHIELD_DECENTRALIZED.COMPLETE === status
+      ) {
         statusColor = COLORS.green;
-      } else if (STATUS_CODE_SHIELD_DECENTRALIZED.TIMED_OUT === status || STATUS_CODE_SHIELD_DECENTRALIZED.TIMED_OUT.includes(status)) {
+      } else if (
+        STATUS_CODE_SHIELD_DECENTRALIZED.TIMED_OUT.includes(status) ||
+        STATUS_CODE_SHIELD_DECENTRALIZED.TIMED_OUT === status
+      ) {
         statusColor = COLORS.orange;
       } else {
         statusColor = COLORS.lightGrey36;
       }
-      break;
-    }
-    default:
-      break;
+    } else {
+      statusColor = COLORS.lightGrey36;
     }
   } catch (error) {
     console.log('getStatusColorShield', error);
   }
-  return statusColor;
+  return statusColor || COLORS.lightGrey36;
 };
 
 export const getStatusColorUnshield = (history) => {
@@ -84,30 +98,23 @@ export const getStatusColorUnshield = (history) => {
       .required()
       .object();
     const { decentralized, status } = history;
-    switch (decentralized) {
-    case 0: {
-      // centralized
-      if (STATUS_CODE_UNSHIELD_CENTRALIZED.COMPLETE === status) {
+    // Check centralized
+    if (CENTRALIZED_ARRAY.includes(decentralized)) {
+      if (STATUS_CODE_UNSHIELD_CENTRALIZED.COMPLETE.includes(status)) {
         statusColor = COLORS.green;
       } else {
         statusColor = COLORS.lightGrey36;
       }
-      break;
     }
-    case 1:
-    case 2:
-    case 3:
-    case 4: {
-      // decetralized
+    // Check decentralized
+    else if (DECENTRALIZE_ARRAY.includes(decentralized)) {
       if (STATUS_CODE_UNSHIELD_DECENTRALIZED.COMPLETE.includes(status)) {
         statusColor = COLORS.green;
       } else {
         statusColor = COLORS.lightGrey36;
       }
-      break;
-    }
-    default:
-      break;
+    } else {
+      statusColor = COLORS.lightGrey36;
     }
   } catch (error) {
     console.log('getStatusColorUnshield', error);
@@ -163,10 +170,13 @@ export const getPortalStatusDetail = (history) => {
       statusDetail = PORTAL_SHIELD_STATUS_DETAIL[status];
       if (status === STATUS_CODE_SHIELD_PORTAL.PROCESSING) {
         const { confirmations } = history;
-        if ( confirmations === undefined ) {
+        if (confirmations === undefined) {
           statusDetail = '';
         } else {
-          statusDetail = statusDetail + confirmations.toString() + '/6 block confirmations.';
+          statusDetail =
+              statusDetail +
+              confirmations.toString() +
+              '/6 block confirmations.';
         }
       }
       break;
