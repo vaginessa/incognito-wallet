f11be242424296bfc94065d40b0c0f569d33f10a
Sang Incognito <72240121+matterhorn063@users.noreply.github.com>
2022-05-18 10:02:05 +0700

    IC-2661 (#2615)
    * [IC-2660] Create new Account similar shardID account Anon
    
    * [IC-2660] Create new Account similar shardID account Anon
    
    * [IC-2603] Hot fix issue ignore passcode
    
    * [IC-2660] Create new Account similar shardID account Anon fix order id
    
    * [IC-2603] Hot fix issue ignore passcode
    
    * [IC-2603] Use async API from RN-randombytes
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    * Improve perf (#2611)
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    Co-authored-by: TN <thu@incognito.org>
    
    * optimize performance shield and add favorite token
    
    * Bugfix (#2612)
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    Co-authored-by: TN <thu@incognito.org>
    
    * optimize performance shield and add favorite token
    
    * Bugfix (#2613)
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    * optimize performance shield and add favorite token
    
    Co-authored-by: TN <thu@incognito.org>
    
    * [IC-2661] fix bug screen lag
    
    * fix bug search token
    
    * fix bug search token
    
    Co-authored-by: darius <datle@incognito.org>
    Co-authored-by: TN <thu@incognito.org>

--- src/components/Token/index.js
+++ src/components/Token/index.js
@@ -5,6 +5,7 @@ export { default as TokenTrade } from './Token.trade';
 export { default as withTokenVerified } from './Token.enhanceVerified';
 export { default as ListToken } from './Token.list';
 export { default as ListAllToken } from './Token.allList';
+export { default as ListAllToken2 } from './Token.allList2';
 export * from './Token.utils';
 export * from './Token.useEffect';
 export { default as TokenFollow } from './Token.follow';
--- src/screens/Shield/Shield.js
+++ src/screens/Shield/Shield.js
@@ -1,37 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@src/components/Header';
 import { BtnQuestionDefault } from '@src/components/Button';
 import PropTypes from 'prop-types';
-import {ListAllToken, TokenFollow} from '@src/components/Token';
+import {
+  ListAllToken2,
+  TokenFollow,
+  handleFilterTokenByKeySearch,
+} from '@src/components/Token';
 import { View } from '@components/core';
 import globalStyled from '@src/theme/theme.styled';
 import { FONT } from '@src/styles';
 import { compose } from 'recompose';
 import withLazy from '@components/LazyHoc/LazyHoc';
+import { isEmpty } from 'lodash';
+import { useFuse } from '@components/Hoc/useFuse';
 import { styled } from './Shield.styled';
 import withShield from './Shield.enhance';
 
 const Shield = (props) => {
-  const { handleShield, handleWhyShield, hideBackButton, ...rest } = props;
+  const { handleShield, handleWhyShield, hideBackButton, availableTokens } =
+    props;
+
+  // Get list verifiedToken list unVerifiedTokens from list all token
+  const _verifiedTokens = availableTokens?.filter((token) => token?.isVerified);
+  const _unVerifiedTokens = availableTokens?.filter(
+    (token) => !token.isVerified,
+  );
+
+  const [showUnVerifiedTokens, setShowUnVerifiedTokens] = useState(false);
+
+  const onSetShowUnVerifiedTokens = () => {
+    setShowUnVerifiedTokens(!showUnVerifiedTokens);
+  };
+
+  const [verifiedTokens, onSearchVerifiedTokens] = useFuse(_verifiedTokens, {
+    keys: ['displayName', 'name', 'symbol', 'pSymbol'],
+    matchAllOnEmptyQuery: true,
+    isCaseSensitive: false,
+    findAllMatches: true,
+    includeMatches: false,
+    includeScore: true,
+    useExtendedSearch: false,
+    threshold: 0,
+    location: 0,
+    distance: 2,
+    maxPatternLength: 32,
+  });
+
+  const [unVerifiedTokens, onSearchUnVerifiedTokens] = useFuse(
+    _unVerifiedTokens,
+    {
+      keys: ['displayName', 'name', 'symbol', 'pSymbol'],
+      matchAllOnEmptyQuery: true,
+      isCaseSensitive: false,
+      findAllMatches: true,
+      includeMatches: false,
+      includeScore: true,
+      useExtendedSearch: false,
+      threshold: 0,
+      location: 0,
+      distance: 2,
+      maxPatternLength: 32,
+    },
+  );
+
+  let tokens = [verifiedTokens];
+  if (showUnVerifiedTokens) {
+    tokens = [verifiedTokens, unVerifiedTokens];
+  }
+
   return (
     <>
       <Header
         title="Search coins"
         canSearch
+        isNormalSearch
+        onTextSearchChange={(value) => {
+          onSearchVerifiedTokens(value);
+          onSearchUnVerifiedTokens(value);
+        }}
         titleStyled={FONT.TEXT.incognitoH4}
         hideBackButton={hideBackButton}
-        rightHeader={(
+        rightHeader={
           <BtnQuestionDefault
             style={{ marginLeft: 8 }}
             onPress={handleWhyShield}
             customStyle={styled.rightItem}
           />
-        )}
+        }
       />
       <View borderTop style={styled.container}>
-        <ListAllToken
-          {...rest}
+        <ListAllToken2
+          tokensFactories={tokens}
           styledCheckBox={globalStyled.defaultPaddingHorizontal}
+          setShowUnVerifiedTokens={onSetShowUnVerifiedTokens}
+          isShowUnVerifiedTokens={showUnVerifiedTokens}
           renderItem={({ item }) => (
             <TokenFollow
               item={item}
@@ -48,17 +111,14 @@ const Shield = (props) => {
 };
 
 Shield.defaultProps = {
-  hideBackButton: false
+  hideBackButton: false,
 };
 
 Shield.propTypes = {
   handleWhyShield: PropTypes.func.isRequired,
   handleShield: PropTypes.func.isRequired,
   tokensFactories: PropTypes.array.isRequired,
-  hideBackButton: PropTypes.bool
+  hideBackButton: PropTypes.bool,
 };
 
-export default compose(
-  withLazy,
-  withShield
-)(Shield);
+export default compose(withLazy, withShield)(Shield);
